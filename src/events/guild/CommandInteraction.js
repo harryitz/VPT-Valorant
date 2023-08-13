62a241dd48a820fc5bf486c34d2573cc5ff5cb2c
taylordevs <taylordevs47@gmail.com>
2023-08-13 16:17:14 +0700

    fix: error unknown interaction

--- src/events/guild/CommandInteraction.js
+++ src/events/guild/CommandInteraction.js
@@ -30,7 +30,6 @@ module.exports = {
             await command.execute(interaction);
         } catch (error) {
             console.error(error);
-            await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
         }
     }
 }
